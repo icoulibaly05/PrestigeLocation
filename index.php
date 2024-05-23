7124af6fda2f68ba5214d141be7894a26f031caa
Imran <imran-abdoulaye.coulibaly@supdevinci-edu.fr>
2024-05-23 15:50:31 +0200

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include('includes/header.php'); ?>
+
+<h1>Bienvenue à LocAuto</h1>
+<p>Gérez votre parc de voitures, vos clients et vos locations facilement.</p>
+
+<ul>
+    <li><a href="voitures.php">Gestion des Voitures</a></li>
+    <li><a href="clients.php">Gestion des Clients</a></li>
+    <li><a href="locations.php">Gestion des Locations</a></li>
+</ul>
+
+<?php include('includes/footer.php'); ?>
